7ec66c83335afa73ea5b0162f2103471ca5d2d71
Alessandra SIMMONS <30960626+MixedMatched@users.noreply.github.com>
2024-12-05 12:34:20 -0500

    finish lean expr definitions in rust (except for syntax lol)

--- rs/lean-parse/src/lean_expr.rs
+++ rs/lean-parse/src/lean_expr.rs
@@ -1,7 +1,16 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "camelCase")]
+pub(crate) enum Literal {
+    NatVal { val: u32 },
+    StrVal { val: String },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub(crate) enum Name {
     Anonymous,
     Str { pre: Box<Name>, str: String },
@@ -44,6 +53,31 @@ pub(crate) enum BinderInfo {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+// hell no.
+pub(crate) enum Syntax {
+    Missing,
+    Node {},
+    Atom {},
+    Ident {},
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) enum DataValue {
+    OfString { v: String },
+    OfBool { v: bool },
+    OfName { v: Name },
+    OfNat { v: u32 },
+    OfInt { v: i32 },
+    OfSyntax { v: Syntax },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct MData(HashMap<Name, DataValue>);
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename = "Expr")]
 #[serde(rename_all = "lowercase")]
 #[serde(rename_all_fields = "camelCase")]
@@ -66,7 +100,7 @@ pub(crate) enum LeanExpr {
     },
     App {
         #[serde(rename = "fn")]
-        fun: Box<LeanExpr>, // needs to be named fn!!
+        function: Box<LeanExpr>, // needs to be named fn!!
         arg: Box<LeanExpr>,
     },
     Lam {
@@ -81,5 +115,25 @@ pub(crate) enum LeanExpr {
         binder_type: Box<LeanExpr>,
         body: Box<LeanExpr>,
         binder_info: BinderInfo,
+    },
+    #[serde(rename = "letE")]
+    LetE {
+        decl_name: Name,
+        #[serde(rename = "type")]
+        typ: Box<LeanExpr>,
+        value: Box<LeanExpr>,
+        body: Box<LeanExpr>,
+        non_dep: bool,
+    },
+    Lit(Literal),
+    MData {
+        data: MData,
+        expr: Box<LeanExpr>,
+    },
+    Proj {
+        type_name: Name,
+        idx: u32,
+        #[serde(rename = "struct")]
+        structure: Box<LeanExpr>,
     },
 }
